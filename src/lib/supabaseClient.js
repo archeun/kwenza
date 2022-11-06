f1c3c020ad3a1ed718a6899a20e72f6cfc35c05b
ArunaTebel <arunatebel@hotmail.com>
2022-11-07 00:51:58 +0530

    Add debug log to check env variables

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,5 @@
 import {createClient} from '@supabase/auth-helpers-sveltekit'
 import {env} from '$env/dynamic/public'
 
+console.log(env)
 export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
