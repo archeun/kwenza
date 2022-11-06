fed30b2dd46413ca8b9b0407b50711fac2dca630
ArunaTebel <arunatebel@hotmail.com>
2022-11-06 22:22:00 +0530

    Add email OTP based authentication for testing purposes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,22 @@
-<script>
-    import "../app.css";
+<script lang="ts">
+    import { supabaseClient } from '$lib/supabaseClient'
+    import { invalidate } from '$app/navigation'
+    import { onMount } from 'svelte'
+    import '../app.css'
+
+    onMount(() => {
+        const {
+            data: { subscription },
+        } = supabaseClient.auth.onAuthStateChange(() => {
+            invalidate('supabase:auth')
+        })
+
+        return () => {
+            subscription.unsubscribe()
+        }
+    })
 </script>
 
-<slot />
+<div class="container" style="padding: 50px 0 100px 0">
+    <slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,16 @@
-<h1 class="text-3xl font-bold underline text-red-600">
-    Hello world!
-</h1>
+<script>
+    import {page} from '$app/stores'
+    import Account from './Account.svelte'
+    import Auth from './Auth.svelte'
+</script>
+
+<svelte:head>
+    <title>Supabase + SvelteKit</title>
+    <meta name="description" content="SvelteKit using supabase-js v2"/>
+</svelte:head>
+
+{#if !$page.data.session}
+    <Auth/>
+{:else}
+    <Account session="{$page.data.session}"/>
+{/if}
