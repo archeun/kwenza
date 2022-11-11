146b4597e4e0fc82600f73232e3dd33f3f9f9543
ArunaTebel <arunatebel@hotmail.com>
2022-11-11 15:24:14 +0530

    Remove Tab index on div element. Replace anchor tag with a div in the left menu

--- src/routes/app/account/+page.svelte
+++ src/routes/app/account/+page.svelte
@@ -55,14 +55,14 @@
     <div class="flex-none w-40 ml-4">
         <ul class="menu text-sm border-r border-r w-40 p-2 h-full">
             <li class="rounded-md mt-4">
-                <a>
+                <div>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                         <path fill-rule="evenodd"
                               d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
                               clip-rule="evenodd"/>
                     </svg>
                     <span>Profile</span>
-                </a>
+                </div>
             </li>
         </ul>
     </div>
